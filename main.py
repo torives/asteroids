e537a447230b5ceb5f2f5e82772e970b152674d9
Victor Yves Crispim <rotcivyves@hotmail.com>
2024-11-26 17:07:41 -0300

    set game to run at 60 fps

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,9 @@ def main():
         
         screen.fill(pygame.Color(0, 0, 0))
         pygame.display.flip()
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60)/1000
     
 
 if __name__ == "__main__":
